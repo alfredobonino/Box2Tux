9395f6454e17d9d51b9bdfb1666de350e8595718
alfredobonino <alfredo.app@me.com>
2014-07-30 17:38:25 +0200

    Create install.sh
    this is the box2tux installer

---
+++ install.sh
@@ -0,0 +1,10 @@
+echo 'Installer for Box2Tux'
+echo '---------------------------------'
+echo 'Installing...'
+sudo apt-get install davfs2
+sudo apt-get install python3 python
+sudo dpkg-reconfigure davfs2
+echo 'copying file..'
+sudo cp box2tux /usr/bin/box2tux
+echo 'installation complete'
+box2tux
