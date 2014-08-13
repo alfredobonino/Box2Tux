fa32775bccfdf48318ef0090f8fa9dfb1656232a
alfredobonino <alfredo.app@me.com>
2014-08-13 14:20:34 +0200

    Update and rename install.sh to install-all.sh
    the install.sh = install-all.sh

--- install-all.sh
+++ install-all.sh
@@ -1,10 +1,6 @@
 echo 'Installer for Box2Tux'
 echo '---------------------------------'
 echo 'Installing...'
-sudo apt-get install davfs2
-sudo apt-get install python3 python
-sudo dpkg-reconfigure davfs2
-echo 'copying file..'
 sudo cp box2tux /usr/bin/box2tux
 echo 'installation complete'
 cd /usr/bin
