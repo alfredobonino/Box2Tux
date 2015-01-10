cd961df5784555b0eddde34804414622afcae514
alfredobonino <alfredo.app@me.com>
2015-01-10 16:44:02 +0100

    Delete install-all.sh

--- install-all.sh
+++
@@ -1,8 +0,0 @@
-echo 'Installer for Box2Tux'
-echo '---------------------------------'
-echo 'Installing...'
-sudo cp box2tux /usr/bin/box2tux
-echo 'installation complete'
-cd /usr/bin
-sudo chmod +x box2tux
-box2tux
